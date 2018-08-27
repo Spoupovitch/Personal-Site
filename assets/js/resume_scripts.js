d45bdb0c219cecf949458f971fd8c04a2421e654
Trevor <trevormjenkins@yahoo.com>
2018-08-26 20:49:44 -0400

    resize background images and thumbnails, create links for project thumbnails, animate bio on page load

--- assets/js/resume_scripts.js
+++ assets/js/resume_scripts.js
@@ -1,3 +1,18 @@
+//load spinner animation
+const spinner = document.getElementById("spinner");
+var ctr = 0;
+var runID = setInterval(function() {
+	ctr -= 5;
+	spinner.style.transform = 'scale(2) rotate(' + ctr + 'deg)';
+}, 15);
+setTimeout(function(){
+	clearInterval(runID);
+}, 1500);
+
+//hide scroll bar
+document.documentElement.style.overflowY = 'hidden';
+
+const loadTime = 1750;
 //load screen animation
 const loadingDiv = document.querySelectorAll("div.load_screen");
 function moveLoadScreen() {
@@ -5,20 +20,11 @@ function moveLoadScreen() {
 		loadingDiv.forEach((loadScreen) => {
 			loadScreen.classList.add('remove');
 		});
-	}, 1750-000);
+		document.documentElement.style.overflowY = 'visible';
+	}, loadTime-00);
+	animateBio();
 }
 window.addEventListener('load', moveLoadScreen());
-
-//load spinner animation
-const spinner = document.getElementById("spinner");
-var ctr = 0;
-var runID = setInterval(function() {
-	ctr += 5;
-	spinner.style.transform = 'scale(3) rotate(' + ctr + 'deg)';
-}, 15);
-setTimeout(function(){
-	clearInterval(runID);
-}, 1500);
 
 //preload themes
 function preloadToggle() {
@@ -32,6 +38,38 @@ function preloadToggle() {
 }
 window.addEventListener('load', preloadToggle());
 
+
+//bio animations
+function animateBio() {
+	const bioWeb1 = document.getElementById("bio_web_1");
+	const bioWeb2 = document.getElementById("bio_web_2");
+	const bioSwe1 = document.getElementById("bio_swe_1");
+	const bioSwe2 = document.getElementById("bio_swe_2");
+
+	//2s delay
+	var staggerBio = 2500;
+	setTimeout(function() {
+		bioWeb1.classList.remove('hide');
+	}, staggerBio);
+
+	//3s delay
+	staggerBio += 1000;
+	setTimeout(function() {
+		bioWeb2.classList.remove('hide');
+	}, staggerBio);
+
+	//5s delay
+	staggerBio += 2000;
+	setTimeout(function() {
+		bioSwe1.classList.remove('hide');
+	}, staggerBio);
+
+	//6s delay
+	staggerBio += 1000;
+	setTimeout(function() {
+		bioSwe2.classList.remove('hide');
+	}, staggerBio);
+}
 
 //toggle site theme
 function toggleBG() {
@@ -89,7 +127,7 @@ function slideProjects(e) {
 		//get bottom of slide element
 		const elemBottom = slideElem.offsetTop + elemHeight;
 
-		const isHalfShown = (slideInAt > slideElem.offsetTop + elemHeight * 1.75);
+		const isHalfShown = (slideInAt > slideElem.offsetTop + elemHeight * 1.5);
 		const isNotScrolledPast = (window.scrollY < elemBottom);
 
 		if (isHalfShown && isNotScrolledPast) {
@@ -114,6 +152,6 @@ $(window).scroll(function () {
 });
 function parallaxScroll() {
 	var wScroll = $(window).scrollTop();
-
-	$('#mid_banner').css('transform', 'translateY(' + -(wScroll / 7.5) + 'px)');
+	
+	$('#mid_banner').css('transform', 'translateY(' + -(wScroll / (14610 / $(window).height()) ) + 'px)');
 }
